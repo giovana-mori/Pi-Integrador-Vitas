855d680025ba323606f9dfa57f947ff2037f9a98
giovana-mori <giovana_mori@outlook.com>
2023-11-25 15:45:50 -0300

    terminar tela de login e registro

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const basico = document.getElementById("ul-basic")
-const listbasic = ["Agendamento", "Cadastro de pacientes", "Confirmação de consultas por Whatsapp", "Lembretes de consultas"]
-
-listbasic.forEach(item => {
-    basico.innerHTML += `
-    <li><img src="static/images/correct.svg" alt="icone correto">${item}</li>
-    `
-});
-
-const plus = document.getElementById("ul-plus")
-const listplus = ["Agendamento", "Cadastro de pacientes", "Confirmação de consultas por Whatsapp", "Lembretes de consultas", "Pedidos de exames", "Prescrição digital"]
-
-listplus.forEach(item => {
-    plus.innerHTML += `
-    <li><img src="static/images/correct.svg" alt="icone correto">${item}</li>
-    `
-});
-
-const premium = document.getElementById("ul-premium")
-const listpremium = ["Agendamento", "Cadastro de pacientes", "Confirmação de consultas por Whatsapp", "Lembretes de consultas", "Pedidos de exames", "Prescrição digital", "Telemedicina", " Marketing digital para Médicos"]
-
-listpremium.forEach(item => {
-    premium.innerHTML += `
-    <li><img src="static/images/correct.svg" alt="icone correto">${item}</li>
-    `
-});
-
 new Glider(document.querySelector('.glider'), {
     slidesToShow: 5,
     slidesToScroll: 5,
@@ -34,4 +7,4 @@ new Glider(document.querySelector('.glider'), {
       prev: '.glider-prev',
       next: '.glider-next'
     }
-  });
+});
