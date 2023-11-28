d11e0af6b72c5c8c82aaf93c17f5f8d2f28eb5b0
andrericardosilva <andre-ricardosilva@outlook.com>
2023-11-27 21:32:47 -0300

    home

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-const basico = document.getElementById("ul-basic")
-const listbasic = ["Agendamento", "Cadastro de pacientes", "Confirmação de consultas por Whatsapp", "Lembretes de consultas"]
-
-listbasic.forEach(item => {
-    basico.innerHTML += `
-    <li><img src="static/images/correct.svg" alt="icone correto">${item}</li>
-    `
-});
-
-const plus = document.getElementById("ul-plus")
-const listplus = ["Agendamento", "Cadastro de pacientes", "Confirmação de consultas por Whatsapp", "Lembretes de consultas", "Pedidos de exames", "Prescrição digital"]
-
-listplus.forEach(item => {
-    plus.innerHTML += `
-    <li><img src="static/images/correct.svg" alt="icone correto">${item}</li>
-    `
-});
-
-const premium = document.getElementById("ul-premium")
-const listpremium = ["Agendamento", "Cadastro de pacientes", "Confirmação de consultas por Whatsapp", "Lembretes de consultas", "Pedidos de exames", "Prescrição digital", "Telemedicina", " Marketing digital para Médicos"]
-
-listpremium.forEach(item => {
-    premium.innerHTML += `
-    <li><img src="static/images/correct.svg" alt="icone correto">${item}</li>
-    `
-});
-
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    draggable: true,
-    dots: '.dots',
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
-  });
+// document.addEventListener('DOMContentLoaded', function () {
+//     new Glider(document.querySelector('.glider'), {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         draggable: true,
+//         dots: '.dots',
+//         arrows: {
+//             prev: '.glider-prev',
+//             next: '.glider-next'
+//         }
+//     });
+// });
